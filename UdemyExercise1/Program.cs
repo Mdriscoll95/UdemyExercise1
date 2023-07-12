01c8bece7d2c0b113fec0a571a3e175dcb20a577
Michael Driscoll <michael.driscoll951@gmail.com>
2023-07-12 16:18:48 -0700

    exercise1

---
+++ UdemyExercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace UdemyExercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string myName = "mike";
+        Console.Write("please enter your name and press enter : ");
+        myName = Console.ReadLine();
+        string myNameUpperCase = string.Format("Upper case : {0}", myName.ToUpper());
+        string myNameLowerCase = string.Format("Lower case : {0}", myName.ToLower());
+        string myNameTrimmed = string.Format("Trimmed value : {0}", myName.Trim());
+        string myNameSubString = string.Format("Substring Vaue :{0}", myName.Substring(0, 5));
+        Console.WriteLine(myNameUpperCase);
+        Console.WriteLine(myNameLowerCase);
+        Console.WriteLine(myNameTrimmed);
+        Console.WriteLine(myNameSubString);
+
+
+        Console.ReadKey();
+    }
+}
+
